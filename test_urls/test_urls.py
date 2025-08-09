3f0ecc2e4358855d90dcf4e2df28226e1ad5272e
Jesús Serrano Sánchez-Toscano <46863457+jesusserranost@users.noreply.github.com>
2025-08-09 17:32:22 +0200

    test_urls: First commit, with a working Python script for testing the URLs from a pool with static file definition and no user input

---
+++ test_urls/test_urls.py
@@ -0,0 +1,20 @@
+import fileinput
+from urllib import request, error
+from sys import stderr
+
+file_name = "pool_urls.txt"
+line_number = 1
+
+for web_address in fileinput.input(file_name):
+    #print(f"{line_number}: {web_address}")
+    request_url = request.Request(url = web_address, headers={'User-Agent': 'Mozilla/6.0'})
+    try:
+        web_content = request.urlopen(request_url)
+        if web_content.status != 200: # https://docs.python.org/3/library/http.html#http-status-codes
+            print(f"ERROR {web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
+        else:
+            print(f"{web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
+    except error.HTTPError as web_error:
+        print(f"FATAL ERROR {web_error.getcode()} {web_error.reason}) while accessing {web_address}", file=stderr)
+    line_number += 1
+
