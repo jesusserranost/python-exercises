ef64ad734d7f915b64ca5b3a68cc2307c61fbb59
Jesús Serrano Sánchez-Toscano <46863457+jesusserranost@users.noreply.github.com>
2025-08-12 16:45:05 +0200

    test_urls: Simplified file access through open() and with-loop structure, instead of fileinput and for, which required an explicit call to close()

--- test_urls/test_urls.py
+++ test_urls/test_urls.py
@@ -1,31 +1,26 @@
-import fileinput
 from urllib import request, error
 from sys import stderr
-
-#file_name = "pool_urls.txt"
 
 def test_urls(filename):
 
     line_number = 1
     error_count = 0
 
-    fi_object = fileinput.input(filename)
+    with open(filename, "r") as file:
 
-    for web_address in fi_object:
-        #print(f"{line_number}: {web_address}")
-        request_url = request.Request(url = web_address, headers={'User-Agent': 'Mozilla/6.0'})
-        try:
-            web_content = request.urlopen(request_url)
-            if web_content.status != 200: # https://docs.python.org/3/library/http.html#http-status-codes
+        for web_address in file:
+            #print(f"{line_number}: {web_address}")
+            request_url = request.Request(url = web_address, headers={'User-Agent': 'Mozilla/6.0'})
+            try:
+                web_content = request.urlopen(request_url)
+                if web_content.status != 200: # https://docs.python.org/3/library/http.html#http-status-codes
+                    error_count += 1
+                    print(f"ERROR {web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
+                else:
+                    print(f"{web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
+            except error.HTTPError as web_error:
                 error_count += 1
-                print(f"ERROR {web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
-            else:
-                print(f"{web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
-        except error.HTTPError as web_error:
-            error_count += 1
-            print(f"FATAL ERROR {web_error.getcode()} {web_error.reason}) while accessing {web_address}", file=stderr)
-        line_number += 1
-
-    fi_object.close()
+                print(f"FATAL ERROR {web_error.getcode()} {web_error.reason}) while accessing {web_address}", file=stderr)
+            line_number += 1
 
     return error_count
