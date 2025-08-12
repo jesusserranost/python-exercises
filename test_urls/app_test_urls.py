75d6c62bcfb2eae89cb7f94bed125ecf338bbaf2
Jesús Serrano Sánchez-Toscano <46863457+jesusserranost@users.noreply.github.com>
2025-08-12 15:57:06 +0200

    test_urls: The function test_urls() now has a return value which is a total number of URLs giving troubles to access, and the app calling it gives now a report to the user about how many failures it encountered

--- test_urls/app_test_urls.py
+++ test_urls/app_test_urls.py
@@ -6,7 +6,12 @@ filename=input("=== Let's test your pool of URLs ===\nGive me a filename where y
 
 if Path(filename).is_file():
     # STEP 3 - Call the function from the external python file
-    test_urls.test_urls(filename)
+    rc = test_urls.test_urls(filename)
+
+    if rc == 1:
+        print(f"There was {rc} inaccessible URL.\ACTION ITEM: Review the report above.", file=stderr)
+    if rc > 1:
+        print(f"There were {rc} inaccessible URLs.\nACTION ITEM: Review the report above.", file=stderr)
 
 else:
     print("ERROR: The filename introduce is not valid or is a directory", file=stderr)
--- test_urls/test_urls.py
+++ test_urls/test_urls.py
@@ -7,6 +7,7 @@ from sys import stderr
 def test_urls(filename):
 
     line_number = 1
+    error_count = 0
 
     for web_address in fileinput.input(filename):
         #print(f"{line_number}: {web_address}")
@@ -14,10 +15,13 @@ def test_urls(filename):
         try:
             web_content = request.urlopen(request_url)
             if web_content.status != 200: # https://docs.python.org/3/library/http.html#http-status-codes
+                error_count += 1
                 print(f"ERROR {web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
             else:
                 print(f"{web_content.getcode()} {web_content.reason} ACCESSING URL #{line_number}: {web_address}")
         except error.HTTPError as web_error:
+            error_count += 1
             print(f"FATAL ERROR {web_error.getcode()} {web_error.reason}) while accessing {web_address}", file=stderr)
         line_number += 1
 
+    return error_count
